86f62ba069f2df291682b1934c62c8e41746a950
tetuya <tsaito@iyell.jp>
2022-12-21 07:25:19 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -111,6 +111,10 @@ resource "azurerm_virtual_machine" "catapp" {
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
 
+  tags = {
+    Department = "devops"
+  }  
+
   storage_image_reference {
     publisher = var.image_publisher
     offer     = var.image_offer
